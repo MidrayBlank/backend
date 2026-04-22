6ec26d24837b86a19528cfbf73c766315e1663d5
Malov Tim <t.malov2014@gmail.com>
2026-04-22 23:34:08 +0300

    POS-44: make router constructor and sending requst

--- src/internal/ai/router.go
+++ src/internal/ai/router.go
@@ -1,7 +1,44 @@
 package ai
 
+import (
+	"fmt"
+	"os"
+)
+
 type QuestionNumber int
 
 type AIRouter struct {
 	client *OpenRouterClient
+	model  string
+}
+
+// NewAIRouter создает объект AIRouter, создавая сначала объект OpenRouterClient
+func NewAIRouter(apiKey, model, baseURL string) *AIRouter {
+	return &AIRouter{
+		client: NewOpenRouterClient(apiKey, baseURL),
+		model:  model,
+	}
+}
+
+// SendRequest создает запрос из полученной строки, затем обращается к OpenRouterClient для обработки и получения результата
+func (r *AIRouter) SendRequest(requstData string) (string, error) {
+	// Создаем стуктуру запросов к LLM
+	message := []Message{
+		{
+			Role:    "user",
+			Content: requstData,
+		},
+	}
+
+	// Создаем обращение к OpenRouter и получаем ответ
+	response, err := r.client.ChatCompletion(r.model, message)
+	if err != nil {
+		// Проверка на timeout у OpenRouterClient
+		if os.IsTimeout(err) {
+			return "", fmt.Errorf("timeout, LLM не ответила за предоставленное ей время: %w", err)
+		}
+		return "", fmt.Errorf("ошибка обращения к LLM: %w", err)
+	}
+
+	return response.Choices[0].Message.Content, nil
 }
