c7144e491fea65b150787d61e402acde72529987
Malov Tim <t.malov2014@gmail.com>
2026-04-22 23:29:30 +0300

    POS-44: delete from interface method GetRequstsBalance

--- src/internal/ai/interface.go
+++ src/internal/ai/interface.go
@@ -1,11 +1,6 @@
 package ai
 
-import "github.com/gofiber/fiber/v3"
-
 type IAIRouter interface {
-	// SendRequestWithQuestion отправляет запрос по номеру вопроса (1,2,3)
-	SendRequestWithQuestion(ctx fiber.Ctx, questionNumber int, inputData string) (string, error)
-
-	// GetRequestsBalance возвращает количество оставшихся запросов
-	GetRequestsBalance() int
+	// SendReques отправляет запрос в LLM по номеру вопроса
+	SendRequest(inputData string) (string, error)
 }
