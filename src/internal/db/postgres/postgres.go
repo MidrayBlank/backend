8f7586eb77e59854e792245c06917e6b18ea5cd4
Malov Tim <t.malov2014@gmail.com>
2026-04-24 02:22:11 +0300

    POS-21: remove DBTransactions

--- src/internal/db/postgres/postgres.go
+++ src/internal/db/postgres/postgres.go
@@ -13,32 +13,28 @@ type PostgresDBConnection struct {
 	conn *gorm.DB
 }
 
-type PostgresDBTransaction struct {
-	tx *gorm.DB
-}
-
-func NewPostgresConnection(dsn string) PostgresDBConnection {
+func NewPostgresConnection(dsn string) abstract.IDBConnection {
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(fmt.Sprintf("Can not connect to postgres DB: %s", err.Error()))
 	}
 
-	return PostgresDBConnection{conn: db}
+	return &PostgresDBConnection{conn: db}
 }
 
 func (db PostgresDBConnection) Get() any {
 	return db.conn
 }
 
-func (db PostgresDBConnection) BeginTx() abstract.IDBTransaction {
-	return PostgresDBTransaction{tx: db.conn.Begin()}
+func (c PostgresDBConnection) BeginTx() abstract.IDBConnection {
+	return &PostgresDBConnection{conn: c.conn.Begin()}
 }
 
-func (db PostgresDBTransaction) Commit() error {
-	return db.tx.Commit().Error
+func (c PostgresDBConnection) Commit() error {
+	return c.conn.Commit().Error
 }
 
-func (db PostgresDBTransaction) Rollback() {
-	db.tx.Rollback()
+func (c PostgresDBConnection) Rollback() {
+	c.conn.Rollback()
 }
