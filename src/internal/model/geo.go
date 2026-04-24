398efe289409ba8bbcc69a04cbb1259dab46d7ab
Malov Tim <t.malov2014@gmail.com>
2026-04-24 15:34:06 +0300

    POS-21: change column admin_type to level

--- src/internal/model/geo.go
+++ src/internal/model/geo.go
@@ -4,5 +4,5 @@ type Geo struct {
 	Code       int    `gorm:"column:code;primaryKey;type:int"`
 	ParentCode int    `gorm:"column:parent_code;type:int;nullable"`
 	Name       string `gorm:"column:name;type:text;not null"`
-	AdminType  int    `gorm:"column:admin_type;type:int;not null"`
+	Level      int    `gorm:"column:level;type:int;not null"`
 }
