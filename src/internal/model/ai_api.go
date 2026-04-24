9da075f4f5e5f61b610cc95cac8cef50b5912bef
Malov Tim <t.malov2014@gmail.com>
2026-04-24 16:04:26 +0300

    POS-21: add method for the gorm - TableName

--- src/internal/model/ai_api.go
+++ src/internal/model/ai_api.go
@@ -1,7 +1,10 @@
 package model
 
-// AIAPI - счётчик запросов по api key, хранящемуся в виде хеша (50 в день, 20 в минуту)
 type AIAPI struct {
 	Hash     string `gorm:"column:hash;primaryKey;type:text"`
 	Requests int    `gorm:"column:requests;type:int;default:0"`
+}
+
+func (AIAPI) TableName() string {
+	return "midray.ai_api"
 }
--- src/internal/model/geo.go
+++ src/internal/model/geo.go
@@ -2,7 +2,11 @@ package model
 
 type Geo struct {
 	Code       int    `gorm:"column:code;primaryKey;type:int"`
-	ParentCode int    `gorm:"column:parent_code;type:int;nullable"`
+	ParentCode *int   `gorm:"column:parent_code;type:int;nullable"`
 	Name       string `gorm:"column:name;type:text;not null"`
 	Level      int    `gorm:"column:level;type:int;not null"`
+}
+
+func (Geo) TableName() string {
+	return "midray.geo"
 }
