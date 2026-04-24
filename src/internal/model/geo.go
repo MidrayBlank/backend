36ed077301a42edfeb9af6ec39f2009f828cbdfe
Malov Tim <t.malov2014@gmail.com>
2026-04-25 00:29:03 +0300

    POS-21: add auto mappers to geo

--- src/internal/model/geo.go
+++ src/internal/model/geo.go
@@ -1,5 +1,11 @@
 package model
 
+import (
+	"backend/src/internal/domain"
+
+	"github.com/jinzhu/copier"
+)
+
 type Geo struct {
 	Code       int    `gorm:"column:code;primaryKey;type:int"`
 	ParentCode *int   `gorm:"column:parent_code;type:int;nullable"`
@@ -9,4 +15,16 @@ type Geo struct {
 
 func (Geo) TableName() string {
 	return "geo"
+}
+
+func (m *Geo) ToDomain() (*domain.Geo, error) {
+	var result domain.Geo
+	if err := copier.Copy(&result, m); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (m *Geo) FromDomainToModel(d *domain.Geo) error {
+	return copier.Copy(m, d)
 }
