f8f9921d26f6030428d1c1be6c874ea29fc14c1a
Malov Tim <t.malov2014@gmail.com>
2026-04-25 00:27:24 +0300

    POS-21: add auto mappers to ai_api

--- src/internal/model/ai_api.go
+++ src/internal/model/ai_api.go
@@ -1,5 +1,11 @@
 package model
 
+import (
+	"backend/src/internal/domain"
+
+	"github.com/jinzhu/copier"
+)
+
 type AIAPI struct {
 	Hash     string `gorm:"column:hash;primaryKey;type:text"`
 	Requests int    `gorm:"column:requests;type:int;default:0"`
@@ -7,4 +13,31 @@ type AIAPI struct {
 
 func (AIAPI) TableName() string {
 	return "ai_api"
+}
+
+func (m *AIAPI) ToDomain() (*domain.AIAPI, error) {
+	var result domain.AIAPI
+	if err := copier.Copy(&result, m); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func (m *AIAPI) FromDomainToModel(d *domain.AIAPI) error {
+	if d == nil {
+		return nil
+	}
+	return copier.Copy(m, d)
+}
+
+func (m AIAPI) ToDomainSlice(daos []AIAPI) ([]domain.AIAPI, error) {
+	result := make([]domain.AIAPI, len(daos))
+	for i, dao := range daos {
+		domainItem, err := dao.ToDomain()
+		if err != nil {
+			return nil, err
+		}
+		result[i] = *domainItem
+	}
+	return result, nil
 }
