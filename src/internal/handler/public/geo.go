41f4cff51ce83a2c18bd998ea9294bb46628f9c9
Malov Tim <t.malov2014@gmail.com>
2026-04-26 23:08:44 +0300

    POS-70: make dto for geo

--- src/internal/handler/public/geo.go
+++ src/internal/handler/public/geo.go
@@ -1,1 +1,24 @@
 package public
+
+import (
+	context "backend/src/internal/context/abstract"
+	"backend/src/internal/domain"
+	"backend/src/internal/dto"
+	service "backend/src/internal/service/abstract"
+)
+
+func GetGeoHandler(ctx context.HandlerContext, params dto.RosstatParams, rosstatService service.IRosstatService) (dto.RosstatResponse, error) {
+	rosstatList, err := rosstatService.GetRosstatByCodes(params.Codes)
+	if err != nil {
+		return nil, err
+	}
+
+	codeMap := make(map[int][]domain.RosstatByYear)
+	for _, data := range rosstatList {
+		codeMap[data.Code] = append(codeMap[data.Code], data)
+	}
+
+	rosstatresponse := buildRosstatResponse(codeMap, params.Fields)
+
+	return rosstatresponse, nil
+}
