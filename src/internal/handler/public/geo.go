42849a06235712de44665105f7c01c913a37ff42
Malov Tim <t.malov2014@gmail.com>
2026-04-27 00:08:02 +0300

    POS-70: make geo handler

--- src/internal/handler/public/geo.go
+++ src/internal/handler/public/geo.go
@@ -5,10 +5,11 @@ import (
 	"backend/src/internal/domain"
 	"backend/src/internal/dto"
 	service "backend/src/internal/service/abstract"
+	"sort"
 )
 
 func GetGeoHandler(ctx context.HandlerContext, geoService service.IGeoService) (dto.GeoResponse, error) {
-	geoList, err := geoService.GetGeoByCodes(nil)
+	geoList, err := geoService.GetGeoByCodes(nil) // nil тк выгружаем ВСЕ geo
 	if err != nil {
 		return dto.GeoResponse{}, err
 	}
@@ -16,6 +17,77 @@ func GetGeoHandler(ctx context.HandlerContext, geoService service.IGeoService) (
 	return buildGeoResponse(geoList)
 }
 
-func buildGeoResponse(geoList []*domain.Geo) (dto.GeoResponse, error) {
+func buildGeoResponse(geoList []domain.Geo) (dto.GeoResponse, error) {
+	federalSubjects, childMap := groupGeoByParent(geoList)
 
+	return dto.GeoResponse{
+		Name:            "Российская Федерация",
+		Code:            0,
+		FederalSubjects: buildFederalSubjects(federalSubjects, childMap),
+	}, nil
+}
+
+func groupGeoByParent(geoList []domain.Geo) ([]domain.Geo, map[int][]domain.Geo) {
+	var federalSubjects []domain.Geo
+	childMap := make(map[int][]domain.Geo)
+
+	for _, geo := range geoList {
+		if geo.ParentCode == nil {
+			federalSubjects = append(federalSubjects, geo)
+		} else {
+			childMap[*geo.ParentCode] = append(childMap[*geo.ParentCode], geo)
+		}
+	}
+	return federalSubjects, childMap
+}
+
+func buildFederalSubjects(federalSubjects []domain.Geo, childMap map[int][]domain.Geo) []dto.FederalSubject {
+	sortGeoByCode(federalSubjects)
+
+	var result []dto.FederalSubject
+	for _, federalSubject := range federalSubjects {
+		federalSubjectDTO := dto.FederalSubject{
+			Name:                federalSubject.Name,
+			Code:                federalSubject.Code,
+			UpperMunicipalities: buildUpperMunicipalities(childMap[federalSubject.Code], childMap),
+		}
+
+		result = append(result, federalSubjectDTO)
+	}
+	return result
+}
+
+func buildUpperMunicipalities(upperMunicipalities []domain.Geo, childMap map[int][]domain.Geo) []dto.UpperMunicipality {
+	sortGeoByCode(upperMunicipalities)
+
+	var result []dto.UpperMunicipality
+	for _, upperMunicipality := range upperMunicipalities {
+		upperMunicipalityDTO := dto.UpperMunicipality{
+			Name:                upperMunicipality.Name,
+			Code:                upperMunicipality.Code,
+			LowerMunicipalities: buildLowerMunicipalities(childMap[upperMunicipality.Code]),
+		}
+		result = append(result, upperMunicipalityDTO)
+	}
+	return result
+}
+
+func buildLowerMunicipalities(lowerMunicipalities []domain.Geo) []dto.LowerMunicipality {
+	sortGeoByCode(lowerMunicipalities)
+
+	var result []dto.LowerMunicipality
+	for _, lowerMunicipality := range lowerMunicipalities {
+		lowerMunicipalityDTO := dto.LowerMunicipality{
+			Name: lowerMunicipality.Name,
+			Code: lowerMunicipality.Code,
+		}
+		result = append(result, lowerMunicipalityDTO)
+	}
+	return result
+}
+
+func sortGeoByCode(geoList []domain.Geo) {
+	sort.Slice(geoList, func(i, j int) bool {
+		return geoList[i].Code < geoList[j].Code
+	})
 }
