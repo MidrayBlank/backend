4dd42f03c56c8dc84e7db5bc40749486edb2d369
Malov Tim <t.malov2014@gmail.com>
2026-04-27 08:23:47 +0300

    POS-53: add dispetcher to main

--- src/cmd/async_backend/main.go
+++ src/cmd/async_backend/main.go
@@ -3,13 +3,19 @@ package async_backend
 import (
 	"backend/src/internal/async/dispatcher"
 	"backend/src/internal/config"
+	"backend/src/internal/db/postgres"
+	rimpl "backend/src/internal/repository/impl"
 	"context"
 	"sync"
+	"time"
 )
 
 func main() {
 	config := config.Load()
-	dispatcher := dispatcher.NewDispatcher(config.MaxWorkersCount)
+
+	conn := postgres.NewPostgresConnection(config.GetDBDSN())
+	requestsRepo := rimpl.NewAsyncRequesRepository()
+	dispatcher := dispatcher.NewDispatcher(conn, *requestsRepo, 5*time.Second, config.MaxAttempts, config.MaxWorkersCount)
 
 	var wg sync.WaitGroup
 	wg.Add(2)
